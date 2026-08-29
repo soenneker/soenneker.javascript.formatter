4b65dd84504adc791f8ccd274bb06aa932700b00
soenneker <jake@soenneker.com>
2026-08-29 14:17:08 -0500

    Improve public API XML documentation

--- src/Soenneker.JavaScript.Formatter/Registrars/JavaScriptFormatterRegistrar.cs
+++ src/Soenneker.JavaScript.Formatter/Registrars/JavaScriptFormatterRegistrar.cs
@@ -14,6 +14,8 @@ public static class JavaScriptFormatterRegistrar
     /// <summary>
     /// Adds <see cref="IJavaScriptFormatter"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddJavaScriptFormatterAsSingleton(this IServiceCollection services)
     {
         services.AddFileUtilAsSingleton().AddDirectoryUtilAsSingleton().TryAddSingleton<IJavaScriptFormatter, JavaScriptFormatter>();
@@ -24,6 +26,8 @@ public static class JavaScriptFormatterRegistrar
     /// <summary>
     /// Adds <see cref="IJavaScriptFormatter"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddJavaScriptFormatterAsScoped(this IServiceCollection services)
     {
         services.AddFileUtilAsScoped().AddDirectoryUtilAsScoped().TryAddScoped<IJavaScriptFormatter, JavaScriptFormatter>();
